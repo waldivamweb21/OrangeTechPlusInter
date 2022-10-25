343b2683701f0315e346345c82f22f5e5a8682e3
waldivamweb21 <waldivamprogramadorweb21@gmail.com>
2022-10-25 20:41:19 -0300

    Primeiro Passos Com JavaScript

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,1 +1,23 @@
-console.log('Sucesso');
+const offset = 0
+const limit = 10
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+// Assincrono: uma hora vai ter a resposta(response) do servidor.
+// Interface de uma Proxy.
+
+fetch(url)
+    .then(function (response){
+        response.json().then(function(responseBody){
+            console.log(responseBody)
+        })
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+    .finally(function () {
+        console.log('Requisição concluída!')
+    })
+
+    //pausa: 08:46.
+
+
